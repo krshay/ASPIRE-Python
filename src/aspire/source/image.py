51c2126b60026d99df3d40eab19dc0b0dc91373b
Garrett Wright <garrettwrong@gmail.com>
2021-06-15 10:52:14 -0400

    develop rebase merge resolve2

--- src/aspire/source/image.py
+++ src/aspire/source/image.py
@@ -230,6 +230,7 @@ class ImageSource:
         self.set_metadata(
             ["_rlnAngleRot", "_rlnAngleTilt", "_rlnAnglePsi"], np.rad2deg(values)
         )
+        self._rotations_set = True
 
     @rots.setter
     def rots(self, values):
@@ -243,6 +244,7 @@ class ImageSource:
             ["_rlnAngleRot", "_rlnAngleTilt", "_rlnAnglePsi"],
             self._rotations.as_euler("ZYZ", degrees=True),
         )
+        self._rotations_set = True
 
     def set_metadata(self, metadata_fields, values, indices=None):
         """
